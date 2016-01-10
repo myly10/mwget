37d24ebe288363e5fe21d42a1af54e7db35f2ebe
myly10 <myly10@gmail.com>
2016-01-11 00:02:49 +0800

    some cleaning

--- mwget-svn/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(myget):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="myget" command="/usr/bin/myget"
--- mwget-svn/debian/myget.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the myget package
-#
-0 4	* * *	root	[ -x /usr/bin/myget_maintenance ] && /usr/bin/myget_maintenance
